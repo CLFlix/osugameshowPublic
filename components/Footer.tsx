fed9cc532a3b959a165744654b15ad322d8e3b61
CLFlix <remcoslootmaekers@gmail.com>
2026-02-15 17:40:42 +0100

    added socials to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 w-full text-center text-lg relative">
-      <Link href="https://osu.ppy.sh/users/18755966">_Kurookami_</Link>
+    <footer className="bg-gray-900 w-full text-lg relative flex gap-9 justify-center">
+      <Link href="https://osu.ppy.sh/users/18755966">osu! Profile</Link>
+      <Link href="https://discord.gg/4HAbQm2tdp">Discord</Link>
+      <p>_Kurookami_</p>
+      <Link href="https://www.twitch.tv/kurookamitv">Twitch</Link>
+      <Link href="https://www.youtube.com/@Doku_Kurookami">YouTube</Link>
     </footer>
   );
 };
